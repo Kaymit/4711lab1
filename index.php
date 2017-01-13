583f70da5b20a577fd10d7f0ab8d5e3211540a5d
Kevin <Kevin@142.232.150.9>
2017-01-12 17:08:03 -0800

    -Cloned empty repo -Edited readme -Added index.php with php script

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>"Excellent Title, Just The Best Title"</title>
+    </head>
+    <body>
+        <?php
+            echo "I am a god!";
+        ?>
+    </body>
+</html>
